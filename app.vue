6ea7ef242f875acc4ad475c1273107754c331514
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-10-14 16:52:30 +0200

    Feat(LenisScroll): Ajout du smoothScroll

--- app.vue
+++ app.vue
@@ -1,4 +1,17 @@
-<script setup lang="ts"></script>
+<script setup>
+import Lenis from "lenis";
+
+onMounted(() => {
+  const lenis = new Lenis();
+
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+});
+</script>
 
 <template>
   <div>
