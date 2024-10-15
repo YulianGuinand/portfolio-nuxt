1d1882ca0968249e47c0781c7cf7f6914db18bb0
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-10-15 11:35:51 +0200

    Feat(Contact): page de contact

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
+<script setup>
+// import VueSweetalert2 from "vue-sweetalert2";
+// import "sweetalert2/dist/sweetalert2.min.css";
+// import App from "../app.vue";
+// const app = createApp(App);
+
+// app.use(VueSweetalert2);
+</script>
+
 <template>
   <AppHero />
   <AppProjects />
   <AppCompetences />
+  <AppContact />
 </template>
