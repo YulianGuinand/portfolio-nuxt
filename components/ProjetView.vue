f8855021a447899336c24977726c511bcb23c04f
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-10-16 14:13:09 +0200

    Fix: UI de la premiere partie de la vue de projet

--- components/ProjetView.vue
+++ components/ProjetView.vue
@@ -3,28 +3,26 @@
     id="projet_section"
     class="mt-[200px] flex flex-col gap-8 lg:mt-[100px]"
   >
-    <div class="flex h-full w-full flex-col gap-8 lg:h-screen">
-      <div class="flex h-full flex-col gap-4 lg:flex-row">
-        <div class="h-full w-full overflow-hidden lg:w-[50%]">
-          <img
-            :src="data.imagePreview"
-            alt="IMG"
-            class="h-full w-full scale-110 object-cover transition-transform hover:scale-100"
-          />
-        </div>
-        <div class="flex h-full w-full items-end px-8 lg:w-[50%] lg:px-0">
-          <div class="flex h-[60%] flex-col gap-4">
-            <h2 class="text-2xl text-white mix-blend-difference lg:text-6xl">
+    <div class="relative flex h-screen w-full flex-col gap-8">
+      <div class="flex h-full flex-col gap-4 bg-cover lg:flex-row">
+        <div
+          class="ml-4 h-[calc(100%-300px)] w-[90%] rounded-lg bg-cover bg-center bg-no-repeat shadow-lg"
+          :style="{ backgroundImage: image }"
+        ></div>
+        <div
+          class="flex w-full items-end rounded-lg bg-white shadow-lg md:absolute md:bottom-[6rem] md:right-[4rem] md:w-3/4 lg:bottom-[8rem] lg:w-1/2 lg:px-0"
+        >
+          <div class="flex h-full flex-col gap-4 p-8 md:h-[60%]">
+            <h2 class="text-2xl text-[#2d2d2d] lg:text-4xl">
               {{ data.title }}<br />
-              {{ data.description }}
+              {{ data.description ? data.description : null }}
             </h2>
-            <p class="w-full max-w-[900px] pr-8">
-              {{ data.detail }}<br />{{ data.detail2 }}<br /><br />{{
-                data.detail3
-              }}
-            </p>
+            <p
+              v-html="data.detail"
+              class="w-full max-w-[900px] pr-8 text-sm lg:text-lg"
+            ></p>
             <div
-              class="mt-4 flex h-[100px] w-full flex-col justify-around sm:flex-row"
+              class="mt-4 flex h-fit w-full flex-col justify-around sm:flex-row"
             >
               <Tags v-for="tag in data.tags" :tag="tag" />
             </div>
@@ -33,7 +31,7 @@
       </div>
     </div>
 
-    <Cards :data="data.imgsLinks" />
+    <Cards v-if="data.imgsLinks" :data="data.imgsLinks" />
   </section>
 </template>
 
@@ -43,4 +41,5 @@
 import Cards from "./Cards.vue";
 
 const props = defineProps(["data"]);
+const image = ref("url(" + props.data.imagePreview + ")");
 </script>
---
+++ error.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+
+import Lenis from "lenis";
+
+onMounted(() => {
+  const lenis = new Lenis();
+
+  function raf(time: any) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+
+  requestAnimationFrame(raf);
+});
+</script>
+
+<template>
+  <NuxtLayout>
+    <main
+      class="mb-[400px] flex h-screen w-full flex-col items-center justify-center gap-8"
+    >
+      <h1 class="text-4xl">
+        Erreur :
+        <span class="uppercase">
+          {{
+            error?.statusCode === 404 ? "quatre-cent-quatre" : error?.statusCode
+          }}
+        </span>
+      </h1>
+      <p>{{ error?.statusMessage }}</p>
+      <NuxtLink
+        to="/"
+        class="w-fit cursor-pointer border-none bg-[#2d2d2d] px-8 py-4 text-sm font-semibold text-white outline-none transition-colors hover:bg-[#414040]"
+        >HOME</NuxtLink
+      >
+    </main>
+  </NuxtLayout>
+</template>
