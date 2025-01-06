2da30d6a56882d5b32f15b72c232106b05029334
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2025-01-06 12:15:11 +0100

    Feat: change favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+useSeoMeta({
+  title: "Portfolio - Yulian Guinand",
+  description: "[description]",
+  ogTitle: "[og:title]",
+  ogDescription: "[og:description]",
+  ogImage: "[og:image]",
+  ogUrl: "[og:url]",
+  twitterTitle: "[twitter:title]",
+  twitterDescription: "[twitter:description]",
+  twitterImage: "[twitter:image]",
+  twitterCard: "summary",
+});
+</script>
 
 <template>
   <main class="z-[999] mb-[400px] scroll-smooth">
